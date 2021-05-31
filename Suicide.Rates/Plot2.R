b17fe2e8d95c0d529b6e11e0fe5e3e04f2cda961
Janey Yee <jyee657@uw.edu>
2021-05-31 15:45:40 -0700

    Started plot 2, modified csv file to delete one column.

--- Suicide.Rates/Plot2.R
+++ Suicide.Rates/Plot2.R
@@ -1,3 +1,19 @@
-output$plot2 <- renderText({
-  paste("This is Plot2")
+suicide.data <- read.csv("Data/Suicide_Rates.csv") 
+
+output$plot2 <- renderPlot({
+  
+  data.male <- suicide.data %>% 
+    filter(X.1 == " Male")
+  
+  data.female <- suicide.data %>% 
+    filter(X.1 == " Female")
+  
+  p <- ggplot(data, aes_string(input$xVar, input$n, fill="Shape"))
+  labs(
+    x = input$xVar,
+    y = "Number of Observations",
+    title = paste(input$xVar, "vs Number of Observations"))
+  p + geom_bar(stat = "identity")
+  
+  
 })
